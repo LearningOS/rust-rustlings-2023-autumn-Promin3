aea5451065ba1a93e48b77a39418cd3ad48f9f5f
Promin <3056063115@qq.com>
2023-10-11 05:08:36 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -14,13 +14,14 @@
 // - Trim the string
 // - Append "bar" to the string a specified amount of times
 // The exact form of this will be:
-// - The input is going to be a Vector of a 2-length tuple,
+// - The input is going to be a Vec of a 2-length tuple,
 //   the first element is the string, the second one is the command.
-// - The output element is going to be a Vector of strings.
+// - The output element is going to be a Vec of strings.
 //
 // No hints this time!
 
-// I AM NOT DONE
+
+
 
 pub enum Command {
     Uppercase,
@@ -30,13 +31,26 @@ pub enum Command {
 
 mod my_module {
     use super::Command;
-
+    
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String>{
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command{
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(size) => {
+                    let mut s = string.clone();
+                    let mut times = *size;
+                    while times > 0 {
+                        s.push_str("bar");
+                        times -= 1;
+                    }
+                    output.push(s);
+                }
+            }
         }
         output
     }
@@ -45,7 +59,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
