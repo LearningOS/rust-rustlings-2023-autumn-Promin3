e6f6707d9d53b3827d1089cbd71a512a675c6b98
Promin <3056063115@qq.com>
2023-10-20 23:23:44 +0800

    update

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -8,10 +8,7 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    // for x in option {
-    //     res += x;
-    // }
-    while let Some(x) = option{
+    if let Some(x) = option{
         res += x;
     }
     println!("{}", res);
